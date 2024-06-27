666671ace3b1c27ae601758c4cefe4999010d220
Dhananjaya <dhananjaya.20244003@iit.ac.lk>
2024-06-27 09:36:44 +0530

    login page

--- PAGE/index.php
+++ PAGE/index.php
@@ -17,7 +17,7 @@ if (isset($_POST['log-submit-btn'])) {
                 VALUES ('$id', '$first_name', '$second_name', '$email', '$password')";
     
         if ($conn->query($sql) === TRUE) {
-            echo "ammo htto hatri";
+            echo "successfully Registerd";
             exit();
         } else {
             echo "Error: " . $sql . "<br>" . $conn->error;
@@ -52,3 +52,28 @@ if (isset($_POST['log-submit-btn'])) {
 
 //     $conn->close();
 // }
+<?php
+if (isset($_POST['log-submit-btn'])) {
+    $conn = mysqli_connect('localhost', 'root', '', 'reachrightsite');
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) == 1) {
+        header("Location: index2.html");
+        exit();
+    } else {
+        echo "Login failed. Please check your email and password.";
+    }
+
+    $conn->close();
+}
+?>
+
